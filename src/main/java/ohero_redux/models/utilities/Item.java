d2a882d7e2913cca7e1042f5522964267bde827b
Nathan <natefintak@gmail.com>
2025-05-26 19:25:00 -0400

    checkpoint

--- src/main/java/ohero_redux/models/utilities/Item.java
+++ src/main/java/ohero_redux/models/utilities/Item.java
@@ -3,9 +3,31 @@ package ohero_redux.models.utilities;
 public abstract class Item {
 
     private String name;
+    private String description;
 
     public Item(String name) {
         this.name = name;
+        this.description = "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String itemSummary() {
+        return this.getName() + "\n" + this.getDescription();
     }
 
 }
