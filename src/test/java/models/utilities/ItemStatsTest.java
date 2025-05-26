b93d91c68c6bb279cb3ab967568fd71629fb7769
Nathan <natefintak@gmail.com>
2025-05-26 18:36:45 -0400

    item stats tests

--- src/test/java/models/utilities/ItemStatsTest.java
+++ src/test/java/models/utilities/ItemStatsTest.java
@@ -15,6 +15,46 @@ public class ItemStatsTest {
         assertNotNull(testStats2);
     }
 
+    @Test
+    public void testDexMod() {
+        assertEquals(1, testStats2.getDexMod());
+        testStats2.setDexMod(2);
+        assertEquals(2, testStats2.getDexMod());
+    }
 
+    @Test
+    public void testEnergyMod() {
+        assertEquals(2, testStats2.getEnergyMod());
+        testStats2.setEnergyMod(3);
+        assertEquals(3, testStats2.getEnergyMod());
+    }
+
+    @Test
+    public void testStrengthMod() {
+        assertEquals(3, testStats2.getStrengthMod());
+        testStats2.setStrengthMod(4);
+        assertEquals(4, testStats2.getStrengthMod());
+    }
+
+    @Test
+    public void testIntellectMod() {
+        assertEquals(4, testStats2.getIntellectMod());
+        testStats2.setIntellectMod(5);
+        assertEquals(5, testStats2.getIntellectMod());
+    }
+
+    @Test
+    public void testHealthMod() {
+        assertEquals(5, testStats2.getTotalHealthMod());
+        testStats2.setTotalHealthMod(6);
+        assertEquals(6, testStats2.getTotalHealthMod());
+    }
+
+    @Test
+    public void testCharismaMod() {
+        assertEquals(6, testStats2.getCharismaMod());
+        testStats2.setCharismaMod(7);
+        assertEquals(7, testStats2.getCharismaMod());
+    }
 
 }
