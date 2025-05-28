dc772a55827d2ea6e0d5549816289d71d3bb64dc
Nathan <natefintak@gmail.com>
2025-05-28 19:37:26 -0400

    finishes supplies tests (for now)

--- src/test/java/models/items/SuppliesTest.java
+++ src/test/java/models/items/SuppliesTest.java
@@ -38,4 +38,42 @@ public class SuppliesTest {
         assertEquals(expectedNum, test.getQuantity());
     }
 
+    @Test
+    public void test3SuppliesNonNullConstruct() {
+        Supplies test = new Supplies("Adventurer's Pack", "Large backpack, can contain 30 lbs of items", 1);
+        String expectedName = "Adventurer's Pack";
+        String expectedDesc = "Large backpack, can contain 30 lbs of items";
+        Integer expectedNum = 1;
+        assertEquals(expectedName, test.getName());
+        assertEquals(expectedDesc, test.getDescription());
+        assertEquals(expectedNum, test.getQuantity());
+    }
+
+    @Test
+    public void testGetSetName() {
+        Supplies test = new Supplies();
+        assertEquals("", test.getName());
+        String expected = "Rations";
+        test.setName(expected);
+        assertEquals(expected, test.getName());
+    }
+
+    @Test
+    public void testGetSetDescription() {
+        Supplies test = new Supplies("Rations");
+        assertEquals("", test.getDescription());
+        String expected = "Daily food rations";
+        test.setDescription(expected);
+        assertEquals(expected, test.getDescription());
+    }
+
+    @Test
+    public void testGetSetQuantity() {
+        Supplies test = new Supplies("Rations", "Daily food rations");
+        assertEquals(0, test.getQuantity());
+        Integer expected = 14;
+        test.setQuantity(expected);
+        assertEquals(expected, test.getQuantity());
+    }
+
 }
