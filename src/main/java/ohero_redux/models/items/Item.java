fc59d5fea2f3bb3d8f2592249f6ee9803a3b4163
Nathan <natefintak@gmail.com>
2025-05-28 20:10:57 -0400

    checkpoint

--- src/main/java/ohero_redux/models/items/Item.java
+++ src/main/java/ohero_redux/models/items/Item.java
@@ -6,12 +6,27 @@ import ohero_redux.models.utilities.ItemStats;
 
 public class Item extends ohero_redux.models.utilities.Item {
 
-
+    private String name;
+    private String description;
+    private Integer quantity;
     private ItemStats stats;
+
+    public Item() {
+        this.name = "";
+        this.description = "";
+        this.quantity = 0;
+        this.stats = new ItemStats();
+    }
 
     public Item(String name) {
         super(name);
+        this.description = "";
+        this.quantity = 0;
         this.stats = new ItemStats();
     }
+
+    //add additional constructors?
+
+    //add override for itemSummary, include item stats
 
 }
