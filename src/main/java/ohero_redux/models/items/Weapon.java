b5b1741f527fc0c66cb84055e2bbcd0eaeb05d0a
NFintak <natefintak@gmail.com>
2025-06-09 13:39:34 -0400

    checkpoint

--- src/main/java/ohero_redux/models/items/Weapon.java
+++ src/main/java/ohero_redux/models/items/Weapon.java
@@ -6,10 +6,22 @@ import ohero_redux.models.utilities.ItemStats;
 
 public class Weapon extends Item {
 
+    private String name;
+    private String description;
+    private Integer quantity;
     private ItemStats stats;
+
+    public Weapon() {
+        this.name = "";
+        this.description = "";
+        this.quantity = 0;
+        this.stats = new ItemStats();
+    }
 
     public Weapon(String name) {
         super(name);
+        this.description = "";
+        this.quantity = 0;
         this.stats = new ItemStats();
     }
 
--- src/test/java/models/items/WeaponTest.java
+++ src/test/java/models/items/WeaponTest.java
@@ -1,4 +1,42 @@
 package models.items;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import ohero_redux.models.items.Weapon;
+import ohero_redux.models.utilities.ItemStats;
+
 public class WeaponTest {
+
+    Weapon dagger = new Weapon();
+
+    @Test
+    public void testNullConstructor() {
+        Weapon test = new Weapon();
+        assertNotNull(test);
+    }
+
+    @Test
+    public void test1NonNullConstructor() {
+        Weapon test = new Weapon("");
+        assertNotNull(test);
+    }
+
+    @Test
+    public void test2NonNullConstructor() {
+//        Weapon test = new Weapon("", "");
+//        assertNotNull(test);
+    }
+
+    @Test
+    public void test3NonNullConstructor() {
+//        Weapon test = new Weapon("", "", 0);
+//        assertNotNull(test);
+    }
+
+    @Test
+    public void test4NonNullConstructor() {
+//        Weapon test = new Weapon("", "", 0, new ItemStats());
+//        assertNotNull(test);
+    }
+
 }
