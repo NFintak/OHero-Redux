e3559b9762bea31540fb89b0078dd5b1cc1e7241
NFintak <natefintak@gmail.com>
2025-06-16 12:19:52 -0400

    start of inventory tests

--- src/test/java/models/utilities/InventoryTest.java
+++ src/test/java/models/utilities/InventoryTest.java
@@ -1,5 +1,9 @@
 package models.utilities;
 
+import ohero_redux.models.items.Armor;
+import ohero_redux.models.items.Items;
+import ohero_redux.models.items.Supplies;
+import ohero_redux.models.items.Weapon;
 import ohero_redux.models.utilities.Inventory;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,6 +13,9 @@ public class InventoryTest {
 
     @BeforeEach
     public void setUp() {
-
+        Armor armor = new Armor();
+        Items item = new Items();
+        Supplies supplies = new Supplies();
+        Weapon weapon = new Weapon();
     }
 }
