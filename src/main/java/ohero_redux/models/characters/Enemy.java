780bfdef398e6e0ed826835b4293e959788b48d8
Nathan <natefintak@gmail.com>
2025-06-23 09:37:16 -0400

    updates enemy char. constructors, tests in progress

--- src/main/java/ohero_redux/models/characters/Enemy.java
+++ src/main/java/ohero_redux/models/characters/Enemy.java
@@ -1,25 +1,36 @@
 package ohero_redux.models.characters;
 
+import ohero_redux.models.utilities.CharStats;
+
 public class Enemy extends Character {
 
     private String name;
     private Long id;
+    private CharStats stats;
     private String type;
 
     public Enemy() {
         this.name = "";
         this.id = 0L;
+        this.stats = new CharStats();
         this.type = "";
     }
 
     public Enemy(String name) {
         super(name);
         this.id = 0L;
+        this.stats = new CharStats();
         this.type = "";
     }
 
     public Enemy(String name, Long id) {
         super(name, id);
+        this.stats = new CharStats();
+        this.type = "";
+    }
+
+    public Enemy(String name, Long id, CharStats stats) {
+        super(name, id, stats);
         this.type = "";
     }
 
--- src/test/java/models/characters/EnemyTest.java
+++ src/test/java/models/characters/EnemyTest.java
@@ -8,4 +8,27 @@ import ohero_redux.models.utilities.Inventory;
 import ohero_redux.models.items.Items;
 
 public class EnemyTest {
+
+    @Test
+    public void testNullConstructor() {
+        Enemy troll = new Enemy();
+        assertNotNull(troll);
+    }
+
+    @Test
+    public void testNonNullConstructor1() {
+        Enemy troll = new Enemy("Blinky");
+        assertNotNull(troll);
+    }
+
+    @Test
+    public void testNonNullConstructor2() {
+        Enemy troll = new Enemy("Blinky", 123L);
+        assertNotNull(troll);
+    }
+
+    @Test
+    public void testNonNullConstructor3() {
+        Enemy troll = new Enemy("Blinky", 123L, new CharStats());
+    }
 }
