0c0efc26ecb8d0aae7e9d3868dbd4f1047ca63a0
Nathan <natefintak@gmail.com>
2025-06-23 17:40:07 -0400

    start of melee enemy tests

--- src/main/java/ohero_redux/models/characters/Melee.java
+++ src/main/java/ohero_redux/models/characters/Melee.java
@@ -1,6 +1,28 @@
 package ohero_redux.models.characters;
 
+import ohero_redux.models.utilities.CharStats;
+import ohero_redux.models.utilities.Inventory;
+
 public class Melee extends Enemy {
+
+    private String name;
+    private CharStats stats;
+    private String description;
+    private Inventory meleeInventory;
+
+    public Melee() {
+        this.name = "";
+        this.stats = new CharStats();
+        this.description = "";
+        this.meleeInventory = new Inventory();
+    }
+
+    public Melee(String name) {
+        super(name);
+        this.stats = new CharStats();
+        this.description = "";
+        this.meleeInventory = new Inventory();
+    }
 
 
 
--- src/test/java/models/characters/MeleeTest.java
+++ src/test/java/models/characters/MeleeTest.java
@@ -9,4 +9,34 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MeleeTest {
+
+    @Test
+    public void testNullConstructor() {
+        Enemy test = new Melee();
+        assertNotNull(test);
+    }
+
+    @Test
+    public void testNonNullConstructor1() {
+        Enemy test = new Melee("Wraith");
+        assertNotNull(test);
+    }
+
+    @Test
+    public void testNonNullConstructor2() {
+//        Enemy test = new Melee("Wraith", new CharStats(1, 2, 3, 4, 5, 6));
+//        assertNotNull(test);
+    }
+
+    @Test
+    public void testNotNullConstructor3() {
+//        Enemy test = new Melee("Wraith", new CharStats(1, 2, 3, 4, 5, 6), "A whispy, translucent figure; it almost looks familiar");
+//        assertNotNull(test);
+    }
+
+    @Test
+    public void testNotNullConstructor4() {
+//        Enemy test = new Melee("Wraith", new CharStats(1, 2, 3, 4, 5, 6), "A whispy, translucent figure; it almost looks familiar", new Inventory());
+//        assertNotNull(test);
+    }
 }
