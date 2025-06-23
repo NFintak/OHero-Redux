882a562876777de474fc14872b5580f5e49e0b3d
Nathan <natefintak@gmail.com>
2025-06-23 09:16:59 -0400

    start of enemy char. tests

--- src/test/java/models/characters/EnemyTest.java
+++ src/test/java/models/characters/EnemyTest.java
@@ -1,4 +1,11 @@
 package models.characters;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import ohero_redux.models.characters.Enemy;
+import ohero_redux.models.utilities.CharStats;
+import ohero_redux.models.utilities.Inventory;
+import ohero_redux.models.items.Items;
+
 public class EnemyTest {
 }
