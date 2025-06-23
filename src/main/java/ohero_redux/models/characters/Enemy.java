071c3a0fceaff83c87ef69e04ec46129cf0af9e8
Nathan <natefintak@gmail.com>
2025-06-23 09:07:03 -0400

    updates enemy char. constructors

--- src/main/java/ohero_redux/models/characters/Enemy.java
+++ src/main/java/ohero_redux/models/characters/Enemy.java
@@ -2,10 +2,29 @@ package ohero_redux.models.characters;
 
 public class Enemy extends Character {
 
+    private String name;
+    private Long id;
     private String type;
 
-    public Enemy(Long id, String name, String enemyType) {
+    public Enemy() {
+        this.name = "";
+        this.id = 0L;
+        this.type = "";
+    }
+
+    public Enemy(String name) {
         super(name);
+        this.id = 0L;
+        this.type = "";
+    }
+
+    public Enemy(String name, Long id) {
+        super(name, id);
+        this.type = "";
+    }
+
+    public Enemy(String name, Long id, String enemyType) {
+        super(name, id);
         this.type = enemyType;
     }
 
