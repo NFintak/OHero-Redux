36bd773151d59c3d58de459b00488a72e79929a4
NFintak <natefintak@gmail.com>
2025-07-02 01:19:08 -0400

    start of boss enemy constructors

--- src/main/java/ohero_redux/models/characters/Boss.java
+++ src/main/java/ohero_redux/models/characters/Boss.java
@@ -1,7 +1,31 @@
 package ohero_redux.models.characters;
 
+import ohero_redux.models.utilities.CharStats;
+import ohero_redux.models.utilities.Inventory;
+import ohero_redux.models.items.Items;
+
 public class Boss extends Enemy {
 
+    private String name;
+    private CharStats stats;
+    private String description;
+    private Inventory inventory;
+    private Items loot;
 
+    public Boss() {
+        this.name = "";
+        this.stats = new CharStats();
+        this.description = "";
+        this.inventory = new Inventory();
+        this.loot = new Items();
+    }
+
+    public Boss(String name) {
+        super(name);
+        this.stats = new CharStats();
+        this.description = "";
+        this.inventory = new Inventory();
+        this.loot = new Items();
+    }
 
 }
