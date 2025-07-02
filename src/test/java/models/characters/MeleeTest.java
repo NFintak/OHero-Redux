4d3ef52e29da310dc150a8b8904b031f631eeb26
NFintak <natefintak@gmail.com>
2025-07-02 00:52:31 -0400

    adds more tests for melee class

--- src/test/java/models/characters/MeleeTest.java
+++ src/test/java/models/characters/MeleeTest.java
@@ -5,10 +5,13 @@ import ohero_redux.models.characters.Enemy;
 import ohero_redux.models.characters.Melee;
 import ohero_redux.models.utilities.CharStats;
 import ohero_redux.models.utilities.Inventory;
+import ohero_redux.models.items.Items;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MeleeTest {
+
+    Items testItem = new Items();
 
     @Test
     public void testNullConstructor() {
@@ -56,6 +59,37 @@ public class MeleeTest {
         CharStats expected = new CharStats(10, 10, 10, 10, 10, 10);
         test.setStats(expected);
         assertEquals(expected, test.getStats());
+    }
+
+    @Test
+    public void testDescGetterSetter() {
+        Melee test = new Melee("Wraith", new CharStats());
+        assertEquals("", test.getDescription());
+        String expected = "A whispy, translucent figure; it almost looks familiar";
+        test.setDescription(expected);
+        assertEquals(expected, test.getDescription());
+    }
+
+    @Test
+    public void testInvenGetterSetter() {
+        Melee test = new Melee();
+        assertNotNull(test.getInventory());
+        Inventory expected = new Inventory();
+        expected.addItem(testItem);
+        test.setInventory(expected);
+        assertEquals(expected, test.getInventory());
+    }
+
+    @Test
+    public void testInstanceOfEnemy() {
+        Melee test = new Melee();
+        assertInstanceOf(Enemy.class, test);
+    }
+
+    @Test
+    public void testInstanceOfCharacter() {
+        Melee test = new Melee();
+        assertInstanceOf(Characters.class, test);
     }
 
 }
