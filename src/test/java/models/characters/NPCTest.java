f8490c37a41edd1000ef92cf285f9451ed9b6f2e
Nathan <natefintak@gmail.com>
2025-07-10 02:07:20 -0400

    start of NPC tests

--- src/test/java/models/characters/NPCTest.java
+++ src/test/java/models/characters/NPCTest.java
@@ -1,4 +1,12 @@
 package models.characters;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import ohero_redux.models.characters.Characters;
+import ohero_redux.models.characters.NonPlayerChar;
+import ohero_redux.models.utilities.CharStats;
+
 public class NPCTest {
+
+
 }
