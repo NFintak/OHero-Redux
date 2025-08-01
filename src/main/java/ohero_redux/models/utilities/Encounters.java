42748faf1efa34096ba1476963eca7b91bd34499
NFintak <natefintak@gmail.com>
2025-07-31 21:23:04 -0400

    adds more notes to encounters class

--- src/main/java/ohero_redux/models/utilities/Encounters.java
+++ src/main/java/ohero_redux/models/utilities/Encounters.java
@@ -2,6 +2,7 @@ package ohero_redux.models.utilities;
 
 //should be able to randomize encounters of varying difficulties
 //end of encounters should have options for where playerChar wants to go next
+//use enums for encounters?
 
 public class Encounters {
 }
